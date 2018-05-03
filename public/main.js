236a52a9fcadc6556fbf6dd2a6410028f813cbc2
Matthew Conley <matthewconley00@gmail.com>
2018-05-03 16:52:47 +0000

    May 3 commit

--- public/main.js
+++ public/main.js
@@ -1,1 +1,18 @@
 /*global $*/
+$(function () {
+
+    $('.team-list li').click(function() {
+       $('#header').css({'height':'70px','overflow':'hidden'});
+       
+       $('#outer-conference-wrap').hover(function() {
+           $('#header').css({'height':'375px'});
+       });
+       
+        var team_name = $(this).attr('id');
+        $(function(){
+            $("#main").load(team_name + ".html"); 
+        });
+       
+    });
+    
+});
