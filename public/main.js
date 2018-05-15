f607cf0acd7a53313d670a2de9815b9a120b8e21
Matthew Conley <matthewconley00@gmail.com>
2018-05-15 22:47:20 +0000

    May 15th

--- public/main.js
+++ public/main.js
@@ -2,11 +2,16 @@
 $(function () {
 
     $('.team-list li').click(function() {
-       $('#header').css({'height':'70px','overflow':'hidden'});
-       
-       $('#outer-conference-wrap').hover(function() {
-           $('#header').css({'height':'375px'});
-       });
+            
+        $('#header').css({'height':'80px','overflow':'hidden'});
+        
+        $('#drop-down').css({'display':'flex'});
+        
+        $('#drop-down-button').on('click', function() {
+            $('#header').css({'height':'435px'});
+            $('#drop-down').css({'display':'none'});
+            $('#drop-down-button').off();
+        });
        
         var team_name = $(this).attr('id');
         $(function(){
