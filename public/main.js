3a065247faaa2e47c2578dde7572bf36cf9b3e42
Matthew Conley <matthewconley00@gmail.com>
2018-05-17 01:43:24 +0000

    gh-pages

--- public/main.js
+++ public/main.js
@@ -3,14 +3,17 @@ $(function () {
 
     $('.team-list li').click(function() {
             
-        $('#header').css({'height':'80px','overflow':'hidden'});
+        $('#header').css({'height':'95px','overflow':'hidden'});
         
         $('#drop-down').css({'display':'flex'});
         
-        $('#drop-down-button').on('click', function() {
-            $('#header').css({'height':'435px'});
+        $('#click-to-view').show();
+        
+        $('#drop-down').on('click', function() {
+            $('#header').css({'height':'450px'});
             $('#drop-down').css({'display':'none'});
-            $('#drop-down-button').off();
+            $('#click-to-view').hide();
+            $('#drop-down').off();
         });
        
         var team_name = $(this).attr('id');
